231c86ffeb778e944279571cab8512cd1f291060
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-19 02:29:22 -0700

    class models jobs

---
+++ lib/models/jobs.dart
@@ -0,0 +1,18 @@
+class Jobs {
+  final String description;
+  final String montant;
+  final String hour;
+  final String logo;
+  final String entreprise;
+  final String ville;
+  final String dureOffret;
+
+  Jobs(
+      {this.description,
+      this.montant,
+      this.hour,
+      this.logo,
+      this.entreprise,
+      this.ville,
+      this.dureOffret});
+}
