65ee4ea78ba1dda28e6c8de63edf9810add6d6ec
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-23 08:18:49 -0700

    jobs widgets

---
+++ lib/views/widgets/jobs_widgets.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:uix/utils/colors.dart';
+import 'package:uix/views/widgets/buttom_app_bar_shadow.dart';
+
+class JobsListWidgts extends StatelessWidget {
+  const JobsListWidgts({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Senior ux desing"),
+      subtitle: Row(
+        children: [
+          Text(
+            "Channel",
+            style: TextStyle(color: WhiteColor),
+          ),
+          Text(
+            "-",
+            style: TextStyle(color: WhiteColor),
+          ),
+          Text(
+            "full time",
+            style: TextStyle(color: WhiteColor),
+          ),
+          Spacer(),
+          Text(
+            "17days",
+            style: TextStyle(
+                color: BlueColor, fontSize: 10.0, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      leading: ButtomAppBarShawdow(
+        backgroung: BlackColor,
+        widget: Icon(
+          Icons.work,
+          color: WhiteColor,
+        ),
+      ),
+    );
+  }
+}
