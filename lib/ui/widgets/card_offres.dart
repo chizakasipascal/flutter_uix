62467bed7169d5adb316236eb0b70e7118cc7883
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-28 09:02:42 -0700

    carte offres integre a list jobs

--- lib/ui/widgets/card_offres.dart
+++ lib/ui/widgets/card_offres.dart
@@ -16,20 +16,19 @@ class CardOffres extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 5),
-      child: Container(
-        width: size.width * .8,
-        height: 180,
-        decoration: BoxDecoration(
-          color: BlueColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Stack(
+        children: [
+          Container(
+            width: size.width * .8,
+            height: 180,
+            decoration: BoxDecoration(
+              color: BlueColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     "${jobs.description}",
@@ -39,87 +38,100 @@ class CardOffres extends StatelessWidget {
                       fontWeight: FontWeight.w800,
                     ),
                   ),
-                  Icon(
-                    Icons.branding_watermark,
-                    color: WhiteColor,
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text(
-                    "${jobs.subdescritpion}",
-                    style: TextStyle(
-                      color: WhiteColor,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Container(
-                    height: 30,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: WhiteColor.withOpacity(.4),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Full time",
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "${jobs.subdescritpion}",
                         style: TextStyle(
                           color: WhiteColor,
                           fontWeight: FontWeight.w800,
                         ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    height: 50,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: WhiteColor.withOpacity(.4),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Icon(
-                      Icons.description,
-                      color: WhiteColor,
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${jobs.corporater}",
-                        style: TextStyle(
-                          color: WhiteColor,
+                      SizedBox(width: 5),
+                      Container(
+                        height: 30,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: WhiteColor.withOpacity(.4),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
-                      Text(
-                        "${jobs.location}",
-                        style: TextStyle(
-                          color: WhiteColor,
+                        child: Center(
+                          child: Text(
+                            "Full time",
+                            style: TextStyle(
+                              color: BlackColor,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
                   Spacer(),
-                  Text(
-                    "${jobs.daysleft}",
-                    style: TextStyle(
-                      color: Colors.red,
-                    ),
-                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: WhiteColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.wallet_membership,
+                          color: BlueColor,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${jobs.corporater}",
+                            style: TextStyle(
+                              color: WhiteColor,
+                            ),
+                          ),
+                          Text(
+                            "${jobs.location}",
+                            style: TextStyle(
+                              color: WhiteColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Text(
+                          "${jobs.daysleft}",
+                          style: TextStyle(
+                            color: WhiteColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            right: 0,
+            child: Container(
+              height: 100,
+              width: 90,
+              decoration: BoxDecoration(
+                color: WhiteColor.withOpacity(.2),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(100),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
