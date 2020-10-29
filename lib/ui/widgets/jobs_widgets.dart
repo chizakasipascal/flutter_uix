c6df8f7f04db482002b1ff3d0cf86ecac27bbca2
Chiza Kasi Pascal <pascalkasichiza@gmail.com>
2020-10-29 08:29:30 -0700

    remove overy widget not used

--- lib/ui/widgets/jobs_widgets.dart
+++ lib/ui/widgets/jobs_widgets.dart
@@ -13,37 +13,35 @@ class JobsListWidgts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(jobs.description),
-      subtitle: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(
-            "${jobs.subdescription}",
-            style: TextStyle(color: BlueColor),
-          ),
-          Text(
-            "-",
-            style: TextStyle(color: BlueColor),
-          ),
-          Text(
-            "${jobs.subdescription2}",
-            style: TextStyle(color: BlueColor),
-          ),
-          Spacer(),
-          Text(
-            "${jobs.daysleft}",
-            style: TextStyle(
-                color: BlueColor, fontSize: 10.0, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-      leading: ButtomAppBarShawdow(
-        backgroung: BlackColor,
-        widget: Icon(
-          Icons.work,
-          color: WhiteColor,
+        title: Text(jobs.description),
+        subtitle: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "${jobs.subdescription}",
+              style: TextStyle(color: BlueColor),
+            ),
+            Text(
+              "-",
+              style: TextStyle(color: BlueColor),
+            ),
+            Text(
+              "${jobs.subdescription2}",
+              style: TextStyle(color: BlueColor),
+            ),
+            Spacer(),
+            Text(
+              "${jobs.daysleft}",
+              style: TextStyle(
+                  color: BlueColor,
+                  fontSize: 10.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
-      ),
-    );
+        leading: CircleAvatar(
+          backgroundColor: BlueColor,
+          child: Placeholder(),
+        ));
   }
 }
